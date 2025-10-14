68d872d15c5aff6b3575c8789546c750b2db84af
peterjaber <peterjaberau@gmail.com>
2025-10-14 22:48:39 +1100

    initial

--- demos/survey-ui/global.d.ts
+++ demos/survey-ui/global.d.ts
@@ -6,3 +6,15 @@ declare module "react" {
     [key: `--${string}`]: string | number
   }
 }
+
+
+declare module '*.svg' {
+  import { FC, SVGProps } from 'react'
+  const content: FC<SVGProps<SVGElement>>
+  export default content
+}
+
+declare module '*.svg?url' {
+  const content: any
+  export default content
+}
--- demos/survey-ui/src/app/page.tsx
+++ demos/survey-ui/src/app/page.tsx
@@ -1,15 +1,74 @@
 "use client"
 import Link from "next/link"
 
-import { Container, Center, Card, Stack, HStack, Text, Icon } from "@chakra-ui/react"
+import { IconButton, Container, Center, Card, Stack, HStack, Text, Icon } from "@chakra-ui/react"
 import { LuArrowRight } from "react-icons/lu"
 import { registryMetadata } from "#app/examples/components"
+import { MenuIcon } from "@dev-kit/icons"
+import { IconNamed } from "#components/icon-named"
+
 
 export default function Page() {
   return (
     <Container fluid w="full" h="full" bg={"bg.subtle"}>
       <Center py={8} >
         <Card.Root size="sm" minWidth={'350px'}>
+          <Card.Header>
+            <Stack>
+              <HStack gap={4}>
+                <MenuIcon />
+
+                <Icon size={'2xl'}>
+                  <MenuIcon />
+                </Icon>
+                <Icon size={'sm'}>
+                  <MenuIcon />
+                </Icon>
+
+                <IconButton size={'2xl'}>
+                  <MenuIcon />
+                </IconButton>
+                <IconButton size={'sm'}>
+                  <MenuIcon />
+                </IconButton>
+
+                <IconButton variant={'ghost'} size={'2xl'}>
+                  <MenuIcon />
+                </IconButton>
+                <IconButton variant={'ghost'} size={'sm'}>
+                  <MenuIcon />
+                </IconButton>
+
+              </HStack>
+              <HStack gap={4}>
+                <IconNamed name={'align-self-stretch-icon'} />
+
+                <Icon size={'2xl'}>
+                  <IconNamed name={'align-self-stretch-icon'} />
+                </Icon>
+                <Icon size={'sm'}>
+                  <IconNamed name={'align-self-stretch-icon'} />
+                </Icon>
+
+                <IconButton size={'2xl'}>
+                  <IconNamed name={'align-self-stretch-icon'} />
+                </IconButton>
+                <IconButton size={'sm'}>
+                  <IconNamed name={'align-self-stretch-icon'} />
+                </IconButton>
+
+                <IconButton variant={'ghost'} size={'2xl'}>
+                  <IconNamed name={'align-self-stretch-icon'} />
+                </IconButton>
+                <IconButton variant={'ghost'} size={'sm'}>
+                  <IconNamed name={'align-self-stretch-icon'} />
+                </IconButton>
+
+
+
+              </HStack>
+            </Stack>
+          </Card.Header>
           <Card.Body>
             <Stack gap="2">
               {registryMetadata.map((registryItem) => (
