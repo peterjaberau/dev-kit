9ac7be1174ea61db060185afc0db6733e80ccdf9
peterjaber <peterjaberau@gmail.com>
2025-11-07 20:53:06 +1100

    cloudflare-template

---
+++ templates/cloudflare/src/actors/model/selectors/current-app.selector.tsx
@@ -0,0 +1,18 @@
+import { useRootActors } from "../hooks"
+import { useSelector } from "@xstate/react"
+
+export const useCurrentApp = () => {
+  const { rootCurrentAppRef: currentAppRef } = useRootActors()
+
+  const sendToCurrentApp = currentAppRef.send
+  const currentAppState: any = useSelector(currentAppRef, (state) => state)
+  const currentAppContext = currentAppState.context
+
+  return {
+    currentAppRef,
+    sendToCurrentApp,
+
+    currentAppState,
+    currentAppContext,
+  }
+}
