529684d2e8eb6221c0e2e549ce915580c295d7a6
peterjaber <peterjaberau@gmail.com>
2026-01-09 20:51:39 +1100

    init

--- demos/dockview/src/app/adaptive/stories/[name]/page.tsx
+++ demos/dockview/src/app/adaptive/stories/[name]/page.tsx
@@ -2,13 +2,37 @@
 
 import { useParams } from "next/navigation"
 import { ComponentRenderer } from "#adaptive-registry"
+import { Container, GridItem, SimpleGrid } from "@chakra-ui/react"
+import DraggableTree from "#drag-and-drop"
+import { dataTree } from "#drag-and-drop/examples/data"
+import { DragDrop } from "#drag-and-drop/components"
 
 export default function Page() {
   const params = useParams()
   const paramValue = params.name as string
   return (
-    <>
-      <ComponentRenderer id={paramValue} />
-    </>
+    <Container width={"full"} backgroundColor={"bg.subtle"} boxShadow={"sm"} p={4} borderRadius={"md"}>
+      <SimpleGrid columns={2} gap={10}>
+        <GridItem>
+          <DraggableTree data={dataTree}>
+            <DragDrop.Root
+              css={{
+                backgroundColor: "bg.panel",
+                width: "320px",
+                border: "1px solid",
+                borderColor: "border",
+                borderRadius: "sm",
+                padding: 3,
+              }}
+            >
+              <DragDrop.Tree />
+            </DragDrop.Root>
+          </DraggableTree>
+        </GridItem>
+        <GridItem>
+          <ComponentRenderer id={paramValue} />
+        </GridItem>
+      </SimpleGrid>
+    </Container>
   )
 }
