97bddb2f66b13e1c52bf94762c07d9b51087482e
peterjaber <peterjaberau@gmail.com>
2026-01-28 02:24:09 +1100

    init

---
+++ demos/json/src/app/adaptive-tool/store/variables.ts
@@ -0,0 +1,9 @@
+export const variables = {
+  global: {
+    current: {
+      location: "home"
+    },
+    locations: ["home", "json", "files", "dev-tools", "debug", "icons", "settings", "profile"]
+  },
+  local: {}
+}
